694980cf35cbf604476bda7dd3dba7bf6dbf196d
SaverioAgostino <97173498+SaverioAgostino@users.noreply.github.com>
2024-05-29 14:48:05 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:uts/screens/auth_page.dart';
+import 'package:uts/screens/welcome.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    const GetMaterialApp(
+      debugShowCheckedModeBanner: false, 
+      home: Welcome()
+    )
+   );
+}
