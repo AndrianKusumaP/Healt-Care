d0e6884470f42a92a288ed8c12eb7fe19202a57b
AndrianKusumaP <andre.kusuma1584@gmail.com>
2024-06-07 00:05:37 +0800

    update

--- lib/screens/register_page.dart
+++ lib/screens/register_page.dart
@@ -75,260 +75,265 @@ class RegisterPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Align(
-          alignment: Alignment.topCenter,
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const SizedBox(height: 20),
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Align(
+            alignment: Alignment.topCenter,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 20),
 
-                // Register text
-                const Text(
-                  'Register',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Inter',
-                    fontSize: 25,
-                  ),
-                ),
-
-                const SizedBox(height: 20),
-
-                // Username field
-                MyTextField(
-                  controller: usernameController,
-                  hintText: 'Username',
-                  prefixIcon: Icons.people,
-                  obscureText: false,
-                ),
-
-                const SizedBox(height: 10),
-
-                // NIK field
-                MyTextField(
-                  controller: nikController,
-                  hintText: 'NIK',
-                  prefixIcon: Icons.card_membership,
-                  obscureText: false,
-                ),
-
-                const SizedBox(height: 10),
-
-                // Email field
-                MyTextField(
-                  controller: emailController,
-                  hintText: 'Email',
-                  prefixIcon: Icons.email,
-                  obscureText: false,
-                ),
-
-                const SizedBox(height: 10),
-
-                // Password field
-                MyTextField(
-                  controller: passwordController,
-                  hintText: 'Password',
-                  prefixIcon: Icons.key,
-                  obscureText: true,
-                ),
-
-                const SizedBox(height: 10),
-
-                // Confirm Password field
-                MyTextField(
-                  controller: confirmpasswordController,
-                  hintText: 'Confirm Password',
-                  prefixIcon: Icons.key,
-                  obscureText: true,
-                ),
-
-                const SizedBox(height: 25),
-
-                // Sign Up button
-                SizedBox(
-                  width: 358,
-                  child: ElevatedButton(
-                    onPressed: () => signUserUp(context),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFF205295),
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 17,
-                      ),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(18),
-                      ),
-                    ),
-                    child: const Text(
-                      "Sign Up",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontFamily: 'Inter',
-                        fontWeight: FontWeight.w400,
-                        height: 0,
-                      ),
+                  // Register text
+                  const Text(
+                    'Register',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Inter',
+                      fontSize: 25,
                     ),
                   ),
-                ),
 
-                const SizedBox(height: 50),
+                  const SizedBox(height: 20),
 
-                // OR divider
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Divider(
-                          thickness: 2,
-                          color: Colors.black,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 10.0),
-                        child: Text(
-                          'OR',
-                          style: TextStyle(color: Colors.black),
-                        ),
-                      ),
-                      Expanded(
-                        child: Divider(
-                          thickness: 2,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ],
+                  // Username field
+                  MyTextField(
+                    controller: usernameController,
+                    hintText: 'Username',
+                    prefixIcon: Icons.people,
+                    obscureText: false,
                   ),
-                ),
 
-                const SizedBox(height: 30),
+                  const SizedBox(height: 10),
 
-                // Social login buttons
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      width: 100,
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.white,
-                          padding: const EdgeInsets.symmetric(
-                            vertical: 17,
-                          ),
-                          shape: RoundedRectangleBorder(
-                            side: const BorderSide(
-                              width: 2,
-                              color: Color(0xCC205295),
-                            ),
-                            borderRadius: BorderRadius.circular(18),
-                          ),
+                  // NIK field
+                  MyTextField(
+                    controller: nikController,
+                    hintText: 'NIK',
+                    prefixIcon: Icons.card_membership,
+                    obscureText: false,
+                  ),
+
+                  const SizedBox(height: 10),
+
+                  // Email field
+                  MyTextField(
+                    controller: emailController,
+                    hintText: 'Email',
+                    prefixIcon: Icons.email,
+                    obscureText: false,
+                  ),
+
+                  const SizedBox(height: 10),
+
+                  // Password field
+                  MyTextField(
+                    controller: passwordController,
+                    hintText: 'Password',
+                    prefixIcon: Icons.key,
+                    obscureText: true,
+                  ),
+
+                  const SizedBox(height: 10),
+
+                  // Confirm Password field
+                  MyTextField(
+                    controller: confirmpasswordController,
+                    hintText: 'Confirm Password',
+                    prefixIcon: Icons.key,
+                    obscureText: true,
+                  ),
+
+                  const SizedBox(height: 25),
+
+                  // Sign Up button
+                  SizedBox(
+                    width: 358,
+                    child: ElevatedButton(
+                      onPressed: () => signUserUp(context),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF205295),
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 17,
                         ),
-                        child: Image.asset(
-                          'assets/images/Ggoogle.webp',
-                          height: 35,
-                          width: 35,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18),
                         ),
                       ),
-                    ),
-                    const SizedBox(width: 20),
-                    SizedBox(
-                      width: 100,
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.white,
-                          padding: const EdgeInsets.symmetric(
-                            vertical: 17,
-                          ),
-                          shape: RoundedRectangleBorder(
-                            side: const BorderSide(
-                              width: 2,
-                              color: Color(0xCC205295),
-                            ),
-                            borderRadius: BorderRadius.circular(18),
-                          ),
-                        ),
-                        child: Image.asset(
-                          'assets/images/apple.png',
-                          height: 35,
-                          width: 35,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 20),
-                    SizedBox(
-                      width: 100,
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.white,
-                          padding: const EdgeInsets.symmetric(
-                            vertical: 17,
-                          ),
-                          shape: RoundedRectangleBorder(
-                            side: const BorderSide(
-                              width: 2,
-                              color: Color(0xCC205295),
-                            ),
-                            borderRadius: BorderRadius.circular(18),
-                          ),
-                        ),
-                        child: Image.asset(
-                          'assets/images/facebook.png',
-                          height: 35,
-                          width: 35,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-
-                const SizedBox(height: 20),
-
-                // Already have an account? Sign In
-                Text.rich(
-                  TextSpan(
-                    children: [
-                      const TextSpan(
-                        text: 'Sudah punya akun? ',
+                      child: const Text(
+                        "Sign Up",
                         style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 14,
-                          fontFamily: 'Poppins',
-                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w400,
                           height: 0,
                         ),
                       ),
-                      TextSpan(
-                        text: 'Sign In',
-                        style: const TextStyle(
-                          color: Color(0xFF205295),
-                          fontSize: 14,
-                          fontFamily: 'Poppins',
-                          fontWeight: FontWeight.w500,
-                          height: 0,
+                    ),
+                  ),
+
+                  const SizedBox(height: 50),
+
+                  // OR divider
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 25.0),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Divider(
+                            thickness: 2,
+                            color: Colors.black,
+                          ),
                         ),
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => LoginPage(),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 10.0),
+                          child: Text(
+                            'OR',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ),
+                        Expanded(
+                          child: Divider(
+                            thickness: 2,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  const SizedBox(height: 30),
+
+                  // Social login buttons
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: 100,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 17,
+                            ),
+                            shape: RoundedRectangleBorder(
+                              side: const BorderSide(
+                                width: 2,
+                                color: Color(0xCC205295),
                               ),
-                            );
-                          },
+                              borderRadius: BorderRadius.circular(18),
+                            ),
+                          ),
+                          child: Image.asset(
+                            'assets/images/Ggoogle.webp',
+                            height: 35,
+                            width: 35,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 20),
+                      SizedBox(
+                        width: 100,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 17,
+                            ),
+                            shape: RoundedRectangleBorder(
+                              side: const BorderSide(
+                                width: 2,
+                                color: Color(0xCC205295),
+                              ),
+                              borderRadius: BorderRadius.circular(18),
+                            ),
+                          ),
+                          child: Image.asset(
+                            'assets/images/apple.png',
+                            height: 35,
+                            width: 35,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 20),
+                      SizedBox(
+                        width: 100,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 17,
+                            ),
+                            shape: RoundedRectangleBorder(
+                              side: const BorderSide(
+                                width: 2,
+                                color: Color(0xCC205295),
+                              ),
+                              borderRadius: BorderRadius.circular(18),
+                            ),
+                          ),
+                          child: Image.asset(
+                            'assets/images/facebook.png',
+                            height: 35,
+                            width: 35,
+                          ),
+                        ),
                       ),
                     ],
                   ),
-                ),
-              ],
+
+                  const SizedBox(height: 20),
+
+                  // Already have an account? Sign In
+                  Text.rich(
+                    TextSpan(
+                      children: [
+                        const TextSpan(
+                          text: 'Sudah punya akun? ',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 14,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w500,
+                            height: 0,
+                          ),
+                        ),
+                        TextSpan(
+                          text: 'Sign In',
+                          style: const TextStyle(
+                            color: Color(0xFF205295),
+                            fontSize: 14,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w500,
+                            height: 0,
+                          ),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => LoginPage(),
+                                ),
+                              );
+                            },
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
