3059f5fe78929bc02b01798369a2913203db719a
AndrianKusumaP <andre.kusuma1584@gmail.com>
2024-06-10 19:41:05 +0800

    update ui doctor menu

--- lib/screens/doctor_menu.dart
+++ lib/screens/doctor_menu.dart
@@ -28,8 +28,18 @@ class DoctorMenu extends StatelessWidget {
 
           return Scaffold(
             appBar: AppBar(
-              title: Text(name),
+              title: Text(
+                name,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              backgroundColor: Colors.blue.shade900,
+              iconTheme: const IconThemeData(
+                color: Colors.white,
+              ),
             ),
+            backgroundColor: const Color.fromARGB(255, 225, 234, 244),
             body: SingleChildScrollView(
               child: Container(
                 margin: const EdgeInsets.only(left: 20.0, right: 20.0),
@@ -42,7 +52,9 @@ class DoctorMenu extends StatelessWidget {
                         image: DecorationImage(
                           image: imageUrl.isNotEmpty
                               ? NetworkImage(imageUrl)
-                              : const AssetImage('assets/images/doctor_profile.jpg') as ImageProvider,
+                              : const AssetImage(
+                                      'assets/images/doctor_profile.jpg')
+                                  as ImageProvider,
                           fit: BoxFit.cover,
                         ),
                         borderRadius: const BorderRadius.only(
@@ -59,7 +71,8 @@ class DoctorMenu extends StatelessWidget {
                                   .withOpacity(0.9),
                               const Color.fromRGBO(32, 82, 149, 1)
                                   .withOpacity(0.3),
-                              const Color.fromRGBO(32, 82, 149, 1).withOpacity(0),
+                              const Color.fromRGBO(32, 82, 149, 1)
+                                  .withOpacity(0),
                             ],
                             begin: Alignment.bottomCenter,
                             end: Alignment.topCenter,
@@ -76,7 +89,8 @@ class DoctorMenu extends StatelessWidget {
                             SizedBox(
                               height: 80,
                               child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
                                 crossAxisAlignment: CrossAxisAlignment.end,
                                 children: [
                                   Column(
@@ -202,135 +216,158 @@ class DoctorMenu extends StatelessWidget {
                             textAlign: TextAlign.justify,
                           ),
                           const SizedBox(
-                          height: 15,
-                        ),
-                        Text(
-                          "Book Date",
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontFamily: 'Poppins',
-                            fontWeight: FontWeight.w600,
-                            color: const Color.fromRGBO(28, 101, 211, 1)
-                                .withOpacity(0.9),
+                            height: 15,
                           ),
-                        ),
-                        const SizedBox(
-                          height: 8,
-                        ),
-                        SizedBox(
-                          height: 70,
-                          child: ListView.builder(
-                              shrinkWrap: true,
-                              scrollDirection: Axis.horizontal,
-                              itemCount: 6,
-                              itemBuilder: (context, index) {
-                                return InkWell(
-                                  child: Container(
-                                    margin: const EdgeInsetsDirectional.symmetric(
-                                        horizontal: 8, vertical: 5),
-                                    padding: const EdgeInsetsDirectional.symmetric(
-                                        vertical: 5, horizontal: 25),
-                                    decoration: BoxDecoration(
-                                        color: index == 1
-                                            ? const Color.fromRGBO(28, 101, 211, 1)
-                                            : const Color(0xFFF2F8FF),
-                                        borderRadius: BorderRadius.circular(5),
-                                        boxShadow: const [
-                                          BoxShadow(
-                                            color: Colors.black12,
-                                            blurRadius: 4,
-                                            spreadRadius: 2,
-                                          )
-                                        ]),
-                                    child: Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        Text(
-                                          "${index + 8}",
-                                          style: TextStyle(
-                                            fontSize: 17,
-                                            color: index == 1
-                                                ? Colors.white
-                                                : Colors.black.withOpacity(0.6),
-                                            fontFamily: 'Poppins',
-                                          ),
-                                        ),
-                                        Text(
-                                          "APR",
-                                          style: TextStyle(
-                                              fontFamily: 'Poppins',
+                          Text(
+                            "Book Date",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w600,
+                              color: const Color.fromRGBO(28, 101, 211, 1)
+                                  .withOpacity(0.9),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 8,
+                          ),
+                          SizedBox(
+                            height: 70,
+                            child: ListView.builder(
+                                shrinkWrap: true,
+                                scrollDirection: Axis.horizontal,
+                                itemCount: 6,
+                                itemBuilder: (context, index) {
+                                  return InkWell(
+                                    child: Container(
+                                      margin:
+                                          const EdgeInsetsDirectional.symmetric(
+                                              horizontal: 8, vertical: 5),
+                                      padding:
+                                          const EdgeInsetsDirectional.symmetric(
+                                              vertical: 5, horizontal: 25),
+                                      decoration: BoxDecoration(
+                                          color: index == 1
+                                              ? const Color.fromRGBO(
+                                                  28, 101, 211, 1)
+                                              : const Color(0xFFF2F8FF),
+                                          borderRadius:
+                                              BorderRadius.circular(5),
+                                          boxShadow: const [
+                                            BoxShadow(
+                                              color: Colors.black12,
+                                              blurRadius: 4,
+                                              spreadRadius: 2,
+                                            )
+                                          ]),
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Text(
+                                            "${index + 8}",
+                                            style: TextStyle(
                                               fontSize: 17,
-                                              fontWeight: FontWeight.w500,
                                               color: index == 1
                                                   ? Colors.white
-                                                  : Colors.black.withOpacity(0.6)),
-                                        )
-                                      ],
+                                                  : Colors.black
+                                                      .withOpacity(0.6),
+                                              fontFamily: 'Poppins',
+                                            ),
+                                          ),
+                                          Text(
+                                            "APR",
+                                            style: TextStyle(
+                                                fontFamily: 'Poppins',
+                                                fontSize: 17,
+                                                fontWeight: FontWeight.w500,
+                                                color: index == 1
+                                                    ? Colors.white
+                                                    : Colors.black
+                                                        .withOpacity(0.6)),
+                                          )
+                                        ],
+                                      ),
                                     ),
-                                  ),
-                                );
-                              }),
-                        ),
-                        const SizedBox(
-                          height: 5,
-                        ),
-                        const Text(
-                          "Book Time",
-                          style: TextStyle(
-                              fontFamily: 'Poppins',
-                              fontSize: 18,
-                              color: Color.fromRGBO(28, 101, 211, 0.6),
-                              fontWeight: FontWeight.w600),
-                        ),
-                        const SizedBox(
-                          height: 8,
-                        ),
-                        SizedBox(
-                          height: 60,
-                          child: ListView.builder(
-                            shrinkWrap: true,
-                            scrollDirection: Axis.horizontal,
-                            itemCount: 11,
-                            itemBuilder: (context, index) {
-                              return Container(
-                                margin: const EdgeInsetsDirectional.symmetric(
-                                    horizontal: 8, vertical: 5),
-                                padding: const EdgeInsetsDirectional.symmetric(
-                                    vertical: 10, horizontal: 10),
-                                decoration: BoxDecoration(
-                                    color: index == 2
-                                        ? const Color.fromRGBO(28, 101, 211, 1)
-                                        : const Color(0xFFF2F8FF),
-                                    borderRadius: BorderRadius.circular(5),
-                                    boxShadow: const [
-                                      BoxShadow(
-                                          color: Colors.black12,
-                                          blurRadius: 4,
-                                          spreadRadius: 2),
-                                    ]),
-                                child: Center(
-                                    child: Text(
-                                  "${index + 8}: 00",
-                                  style: TextStyle(
-                                    fontFamily: "Poppins",
-                                    fontSize: 17,
-                                    color: index == 2
-                                        ? Colors.white
-                                        : Colors.black.withOpacity(0.6),
-                                  ),
-                                )),
-                              );
-                            },
+                                  );
+                                }),
                           ),
-                        ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          const Text(
+                            "Book Time",
+                            style: TextStyle(
+                                fontFamily: 'Poppins',
+                                fontSize: 18,
+                                color: Color.fromRGBO(28, 101, 211, 0.6),
+                                fontWeight: FontWeight.w600),
+                          ),
+                          const SizedBox(
+                            height: 8,
+                          ),
+                          SizedBox(
+                            height: 60,
+                            child: ListView.builder(
+                              shrinkWrap: true,
+                              scrollDirection: Axis.horizontal,
+                              itemCount: 11,
+                              itemBuilder: (context, index) {
+                                return Container(
+                                  margin: const EdgeInsetsDirectional.symmetric(
+                                      horizontal: 8, vertical: 5),
+                                  padding:
+                                      const EdgeInsetsDirectional.symmetric(
+                                          vertical: 10, horizontal: 10),
+                                  decoration: BoxDecoration(
+                                      color: index == 2
+                                          ? const Color.fromRGBO(
+                                              28, 101, 211, 1)
+                                          : const Color(0xFFF2F8FF),
+                                      borderRadius: BorderRadius.circular(5),
+                                      boxShadow: const [
+                                        BoxShadow(
+                                            color: Colors.black12,
+                                            blurRadius: 4,
+                                            spreadRadius: 2),
+                                      ]),
+                                  child: Center(
+                                      child: Text(
+                                    "${index + 8}: 00",
+                                    style: TextStyle(
+                                      fontFamily: "Poppins",
+                                      fontSize: 17,
+                                      color: index == 2
+                                          ? Colors.white
+                                          : Colors.black.withOpacity(0.6),
+                                    ),
+                                  )),
+                                );
+                              },
+                            ),
+                          ),
                           const SizedBox(height: 20.0),
-                    ElevatedButton(
-                      onPressed: () {
-                        
-                      },
-                      child: const Text('Book Appointment'),
-                    ),
+                          Material(
+                            color: const Color.fromRGBO(28, 101, 211, 1),
+                            borderRadius: BorderRadius.circular(10),
+                            child: InkWell(
+                              onTap: () {},
+                              child: SizedBox(
+                                height: 60,
+                                width: MediaQuery.of(context).size.width,
+                                child: const Center(
+                                  child: Text(
+                                    "Book Appointment",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontFamily: "Poppins",
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
                         ],
                       ),
                     ),
--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:Health_Care/screens/doctor_menu.dart';
 import 'package:Health_Care/screens/medicine.dart';
-import 'package:get/get.dart';
 
 class homepage extends StatefulWidget {
   homepage({super.key});
